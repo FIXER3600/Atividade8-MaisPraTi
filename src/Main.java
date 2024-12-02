86d3e3fd793b962079101368d337b5139ff4d9bd
FIXER3600 <guilhermecomercial51@gmail.com>
2024-12-02 20:12:20 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        System.out.println(calculadora.calcular(5,2,'-'));
+
+    }
+}
